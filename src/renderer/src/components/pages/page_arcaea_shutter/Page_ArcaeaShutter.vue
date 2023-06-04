cdd9029e3f715c45f8952a481ae782c619d32eca
Eyre_S <sukazyo@outlook.com>
2023-06-04 18:21:49 +0800

    added themed arcaea shutter

--- src/renderer/src/components/pages/page_arcaea_shutter/Page_ArcaeaShutter.vue
+++ src/renderer/src/components/pages/page_arcaea_shutter/Page_ArcaeaShutter.vue
@@ -2,45 +2,20 @@
 
 import PageCard from '@renderer/components/util/page/PageCard.vue';
 import H1 from '@renderer/components/util/page/H1.vue';
+import P from '@renderer/components/util/page/P.vue';
 import InputButton from '@renderer/components/util/controller/InputButton.vue';
-import { ComponentPublicInstance, StyleValue, computed, ref, watchEffect } from 'vue';
+import ArcaeaShutterBox from './ArcaeaShutterBox.vue';
+import { ref } from 'vue';
+import InputText from '@renderer/components/util/controller/InputText.vue';
 
-const shutter_width_all = ref(1280);
-const shutter_width_left = ref(1216);
-const shutter_width_right = ref(425);
+const shutter = ref<InstanceType<typeof ArcaeaShutterBox>|null>(null);
+const shutter_theme = ref<null|'finale'|'fractureray'|'grievouslady'|'tempestissimo'>(null);
 
-const shutter_container = ref<ComponentPublicInstance|null>(null);
-const shutter_width = ref(0);
-function update_width () {
-	if (shutter_container.value) {
-		shutter_width.value = shutter_container.value.$el.offsetWidth
-	} else {
-		shutter_width.value = 0
-	}
-}
-watchEffect(() => {
-	update_width();
-})
-window.onresize = update_width;
+const shutter_timeout_ms = ref(2000);
 
-const shutter_left_style = computed<StyleValue>(() => { return {
-	width: ((shutter_width.value / shutter_width_all.value) * shutter_width_left.value)+"px"
-}})
-
-const shutter_right_style = computed<StyleValue>(() => { return {
-	width: ((shutter_width.value / shutter_width_all.value) * shutter_width_right.value)+"px"
-}})
-
-// ---
-
-const on_transiton = ref(false);
-
-function start () {
-	on_transiton.value = true;
-}
-
-function stop () {
-	on_transiton.value = false;
+function change_theme (theme: null|'finale'|'fractureray'|'grievouslady'|'tempestissimo') {
+	shutter_theme.value = theme;
+	shutter.value?.set_timeout();
 }
 
 </script>
@@ -49,56 +24,43 @@ function stop () {
 	
 	<PageCard>
 		
-		<H1>Arcaea Shutter</H1>
+		<H1>Arcaea Shutter - Splash Animation Demo</H1>
 		
-		<InputButton @click="start">Start Animation</InputButton>&nbsp;
-		<InputButton @click="stop">End Animation</InputButton>
+		<P>Set Theme <small>(current: {{ shutter_theme?shutter_theme:"default" }})</small></P>
+		<div class="button-set">
+			<InputButton @click="change_theme(null)">default</InputButton>
+			<InputButton @click="change_theme('fractureray')">Fracture Ray</InputButton>
+			<InputButton @click="change_theme('grievouslady')">Grievous Lady</InputButton>
+			<InputButton @click="change_theme('tempestissimo')">Tempestissimo</InputButton>
+			<InputButton @click="change_theme('finale')">FINALE</InputButton>
+		</div>
+		
+		
+		<P>Animations</P>
+		<div class="button-set">
+			<InputButton @click="shutter?.set_on()">Start Animation</InputButton>
+			<InputButton @click="shutter?.set_off()">End Animation</InputButton>
+		</div>
+		<P>Timeout Animations</P>
+		<div class="button-set">
+			<div><InputText v-model="shutter_timeout_ms"></InputText></div>
+			<InputButton @click="shutter?.set_timeout(shutter_timeout_ms)">Start Timeout</InputButton>
+		</div>
 		
 	</PageCard>
 	
-	<PageCard no-padding
-			:class="['preview', on_transiton?'on':'off']"
-			ref="shutter_container" :custom-status="shutter_width">
-		<img class="left" :style="shutter_left_style" src="@renderer/assets/arcaea/shutter_l.png">
-		<img class="right" :style="shutter_right_style" src="@renderer/assets/arcaea/shutter_r.png">
-	</PageCard>
+	<ArcaeaShutterBox ref="shutter" :theme="shutter_theme"></ArcaeaShutterBox>
 	
 </template>
 
 <style lang="less" scoped>
 
-.preview {
-	height: 340px;
-	background-image: url("@renderer/assets/arcaea/testify.jpg");
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
+.button-set {
 	
-	position: relative;
-	overflow: hidden;
-	
-	> img {
-		
-		position: absolute;
-		transition: transform 800ms;
-		
-		&.left {
-			z-index: 901;
-			left: 0;
-			transform: translate(-100%);
-		}
-		
-		&.right {
-			z-index: 900;
-			right: 0;
-			transform: translate(100%);
-		}
-		
-	}
-	
-	&.on > img {
-		transform: translate(0%) !important;
-	}
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 1em;
 	
 }
 
