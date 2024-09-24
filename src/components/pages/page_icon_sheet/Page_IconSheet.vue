4404db0dc1673577a5f63333dcb1334045d9c50a
Eyre_S <sukazyo@outlook.com>
2024-09-24 23:48:06 +0800

    for icon sheet, added page size limit to reduce renderer lag

--- src/components/pages/page_icon_sheet/Page_IconSheet.vue
+++ src/components/pages/page_icon_sheet/Page_IconSheet.vue
@@ -8,7 +8,7 @@ import H1 from "@/components/util/page/H1.vue";
 import P from "@/components/util/page/P.vue";
 import A from "@/components/util/page/A.vue";
 
-import { computed, reactive, ref } from 'vue';
+import { computed, reactive, ref, watchEffect } from 'vue';
 import { Icon } from "./icon";
 import { refDebounced } from '@vueuse/core';
 
@@ -17,6 +17,8 @@ import { refDebounced } from '@vueuse/core';
 // source: https://github.com/ryanoasis/nerd-fonts/blob/master/glyphnames.json
 
 import icons_metadata from "./glyphnames.json";
+import InputButton from '@/components/util/controller/InputButton.vue';
+import toast from '@/components/app_cover/toast/toast';
 
 const available_icons: Icon[] = reactive([]);
 new Promise(() => {
@@ -31,30 +33,83 @@ new Promise(() => {
 })
 
 // There's a delayed_input that will change only when the input
-// have not changed after 500ms.
+// have not changed after 300ms.
 //
 // it can reduce too much caculated on each character inputed.
 
-const input_id = ref("");
-const delayed_input = refDebounced(input_id, 400);
+const input_apply_delay_ms = 300
 
+// There's a page size that will limit how much the icons will
+// be shows at first or after click 'Show more', to reduce the
+// renderer lag caused by amount of icons.
+
+const page_size_on_init = 200
+const page_size_on_more = page_size_on_init
+
+const input_id = ref("");
+const delayed_input = refDebounced(input_id, input_apply_delay_ms);
+
+const icons_listing_all = ref<Icon[]>([])
+const icons_listing_count = ref<number>(page_size_on_init)
 const icons_listing = computed<Icon[]>(() => {
-	return available_icons.filter(v => {
-		
-		var filter = delayed_input.value;
-		if (filter.startsWith('nf-')) filter = filter.substring(3);
-		else if (filter.startsWith('\\u')) filter = filter.substring(2);
-		
-		if (filter == '')
-			return v.name == 'fa-500px'
-		else return (
-			v.name.includes(filter) ||
-			v.char === filter ||
-			v.code.startsWith(filter)
-		)
-		
-	})
+	return icons_listing_all.value.slice(0, icons_listing_count.value)
 })
+const there_are_more = computed<boolean>(() => {
+	return icons_listing.value.length < icons_listing_all.value.length
+	// return true
+})
+
+const defaultsShowingIcons: string[] = [
+	"fa-fonticons",
+	"md-material_design",
+	"weather-night_snow_wind",
+	"dev-code",
+	"oct-copilot",
+	"cod-azure",
+	"linux-archlinux",
+	"pom-pomodoro_ticking",
+	"iec-power",
+	"seti-c_sharp"
+]
+
+watchEffect(() => {
+	
+	let icon_listing_temp: Icon[] = []
+	
+	var filter = delayed_input.value;
+	if (filter.startsWith('nf-')) filter = filter.substring(3);
+	else if (filter.startsWith('\\u')) filter = filter.substring(2);
+	
+	for (const icon of available_icons) {
+		
+		if (filter == '') {
+			
+			if (defaultsShowingIcons.includes(icon.name)) {
+				icon_listing_temp.push(icon);
+			}
+			
+			continue
+			
+		}
+		
+		if (
+			icon.name.includes(filter) ||
+			icon.char === filter ||
+			icon.code.startsWith(filter)
+		) {
+			icon_listing_temp.push(icon);
+		}
+		
+	}
+	
+	icons_listing_all.value = icon_listing_temp
+	icons_listing_count.value = page_size_on_init
+	
+})
+
+function loadMore () {
+	icons_listing_count.value += page_size_on_more
+}
 
 </script>
 
@@ -66,12 +121,38 @@ const icons_listing = computed<Icon[]>(() => {
 		
 		<P>
 			<AboutVersionTag
-			icon="nf-md-simple_icons"
+			icon="nf-oct-smiley"
 			name="Nerd Font Icons"
 			:version="icons_metadata.METADATA.version"></AboutVersionTag>
-			在 <A href="https://www.nerdfonts.com/cheat-sheet">Nerd Fonts</A> 中搜寻你想要的图标。
+			&nbsp;
+			<AboutVersionTag
+			icon="nf-fa-fonticons"
+			name="Font Awesome"
+			version="by NerdFonts"></AboutVersionTag>
+			&nbsp;
+			<AboutVersionTag
+			icon="nf-md-material_design"
+			name="Material Design Icons"
+			version="by NerdFonts"></AboutVersionTag><br>
 		</P>
 		
+		<P>
+			在 <A href="https://www.nerdfonts.com/cheat-sheet">Nerd Fonts</A> 中搜寻你想要的图标。Nerd Fonts
+			自带有 Font Awesome，Material Design Icons，Octicons，Codicons 等各种你喜欢的图标库的图标支援！
+		</P>
+		
+		<P>
+			要注意的是由于 WebView 的限制，一次性加载过多的图标可能导致（严重的）性能问题。我们已经设计了通过每次只加载
+			200 个图标的方式以避免这个问题，不过这个问题仍然可能在使用过多次 "Load More" 的情况下出现。
+		</P>
+		
+		<P>
+			<AboutVersionTag
+			icon="nf-md-simple_icons"
+			name="Simple Icons"
+			version="13.11.0"></AboutVersionTag>
+			将计划在下个版本中加入 <A href="https://simpleicons.org/">Simple Icons</A> 的支持。
+		</P>
 		
 		<div class="blank"></div>
 		
@@ -83,6 +164,10 @@ const icons_listing = computed<Icon[]>(() => {
 		
 		<IconPreview v-for="i of icons_listing" :icon="i"></IconPreview>
 		
+	</PageCard>
+	
+	<PageCard no-bg v-if="there_are_more" class="icon-more">
+		<InputButton @click="loadMore()">Load More</InputButton>
 	</PageCard>
 	
 </template>
@@ -107,6 +192,14 @@ const icons_listing = computed<Icon[]>(() => {
 	flex-direction: row;
 	flex-wrap: wrap;
 	gap: 10px;
+	
+}
+
+.icon-more {
+	
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	
 }
 
